2fd92357039758ec4e40ddad67f084e1c1a7eb82
Tekklos9 <danielplazaplaza99@gmail.com>
2026-07-08 11:17:33 +0200

    Primer commit

---
+++ src/app/components/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { CharacterService } from '../services/Character.service';
+import { Character } from '../models/Character.model';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  characters = signal<Character[]>([]);
+  cargando = signal(true);
+  error = signal('');
+
+  constructor(private characterService: CharacterService) {}
+
+  ngOnInit(): void {
+    this.characterService.getCharacters().subscribe({
+      next: (data) => {
+        this.characters.set(data);
+        this.cargando.set(false);
+      },
+      error: () => {
+        this.error.set('No se pudieron cargar los personajes.');
+        this.cargando.set(false);
+      },
+    });
+  }
+}
